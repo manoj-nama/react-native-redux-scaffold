005c35dadea7facec89f5a4e3b336fec39418a12
Manoj Nama <manoj.nama@outlook.com>
2016-07-02 11:15:20 +0530

    Using styles from common theme base

--- src/containers/app.container.js
+++ src/containers/app.container.js
@@ -4,10 +4,11 @@ import React, {
 	Component,
 } from 'react';
 import {
-	StyleSheet,
 	Text,
 	View,
 } from 'react-native';
+
+import { containerStyle as styles } from '../styles';
 
 export default class App extends Component {
 	constructor(props) {
@@ -24,22 +25,3 @@ export default class App extends Component {
 		)
 	}
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
