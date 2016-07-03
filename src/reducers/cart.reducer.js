80208df340a48417593171adbed246c428aad897
Manoj Nama <manoj.nama@outlook.com>
2016-07-03 22:13:19 +0530

    Adding cart reducer

---
+++ src/reducers/cart.reducer.js
@@ -0,0 +1,11 @@
+import * as actions from '../actions';
+
+const intialState = {
+	products: []
+};
+
+const cartReducer = function(state = intialState, action) {
+	return state;
+}
+
+export default cartReducer;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import productsReducer from './products.reducer';
 import navigationReducer from './navigation.reducer';
 import userReducer from './user.reducer';
+import cartReducer from './cart.reducer';
 
 const rootReducer = combineReducers({
 	products: productsReducer,
 	navigation: navigationReducer,
 	user: userReducer,
+	cart: cartReducer,
 });
 
 export default rootReducer;
