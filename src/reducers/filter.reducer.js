f45004fbee40b2117daf489763c4fa7807ca5942
Manoj Nama <manoj.nama@outlook.com>
2016-07-03 22:13:57 +0530

    Adding filters reducer

---
+++ src/reducers/filter.reducer.js
@@ -0,0 +1,10 @@
+import * as actions from '../actions';
+
+const intialState = {
+};
+
+const filterReducer = function(state = intialState, action) {
+	return state;
+}
+
+export default filterReducer;
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,12 +3,14 @@ import productsReducer from './products.reducer';
 import navigationReducer from './navigation.reducer';
 import userReducer from './user.reducer';
 import cartReducer from './cart.reducer';
+import filterReducer from './filter.reducer';
 
 const rootReducer = combineReducers({
 	products: productsReducer,
 	navigation: navigationReducer,
 	user: userReducer,
 	cart: cartReducer,
+	filter: filterReducer,
 });
 
 export default rootReducer;
