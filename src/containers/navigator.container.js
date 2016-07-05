d8c6e2b05faf8ca04e58b077b33ec667d4d4a0b0
Manoj Nama <manoj.nama@outlook.com>
2016-07-05 21:10:37 +0530

    Adding navbar to the main navigator

--- src/containers/navigator.container.js
+++ src/containers/navigator.container.js
@@ -3,6 +3,9 @@ import {
 	Navigator,
 	View,
 	Text,
+	StatusBar,
+	TouchableHighlight,
+	StyleSheet,
 } from 'react-native';
 
 import HomeComponent from '../components/home.component';
@@ -27,15 +30,65 @@ export default class Nav extends Component {
 
 	render() {
 		const initialRoute = {
-			name: 'Home',
+			name: 'home',
+			title: 'E-Commerce',
 			component: HomeComponent
 		};
+
+		var NavigationBarRouteMapper = {
+			LeftButton(route, navigator, index, navState) {
+				if (index > 0) {
+					return (
+						<TouchableHighlight
+							underlayColor="transparent"
+							onPress={() => { if (index > 0) { navigator.pop() } } }>
+							<Text style={ styles.leftNavButtonText }>Back</Text>
+						</TouchableHighlight>)
+				}
+				else { return null }
+			},
+			RightButton(route, navigator, index, navState) {
+				if (route.onPress) return (
+					<TouchableHighlight
+						onPress={ () => route.onPress() }>
+						<Text style={ styles.rightNavButtonText }>
+              { route.rightText || '>' }
+						</Text>
+					</TouchableHighlight>)
+			},
+			Title(route, navigator, index, navState) {
+				return <Text style={ styles.title }>{route.title}</Text>
+			}
+		};
+
 		return (
-			<Navigator
-				style={{ flex: 1 }}
-				configureScene={ this.configureScene }
-				initialRoute={ initialRoute }
-				renderScene={ this.renderScene } />
+			<View style={{ flex: 1 }}>
+				<StatusBar
+					hidden={true}
+					backgroundColor="blue"
+					barStyle="default"
+					/>
+				<Navigator
+					style={{ flex: 1 }}
+					navigationBar={
+						<Navigator.NavigationBar
+							style={ styles.nav }
+							routeMapper={ NavigationBarRouteMapper } />
+					}
+					configureScene={ this.configureScene }
+					initialRoute={ initialRoute }
+					renderScene={ this.renderScene } />
+			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	nav: {
+		backgroundColor: '#eee'
+	},
+	title: {
+		fontWeight: '600',
+		paddingVertical: 5,
+	}
+});
